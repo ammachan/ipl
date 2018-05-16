3729b3c72fbf219e33029d9b7aab142a1a8999b5
ammachan <34169316+ammachan@users.noreply.github.com>
2018-05-16 15:54:01 +0530

    Update ipl2.py

--- ipl2.py
+++ ipl2.py
@@ -2,7 +2,7 @@ import copy
 import operator
 import re
 
-CURRENT_MATCH = 41
+CURRENT_MATCH = 50
 
 
 class Team:
@@ -19,28 +19,32 @@ class Team:
 class Constants:
     MAX_FIXTURE = 56
 
-    points = {Team.CSK: 14,
-              Team.SRH: 16,
-              Team.RCB: 6,
-              Team.MI: 8,
+    points = {Team.CSK: 16,
+              Team.SRH: 18,
+              Team.RCB: 10,
+              Team.MI: 10,
               Team.KXIP: 12,
               Team.DD: 6,
-              Team.KKR: 10,
-              Team.RR: 8}
+              Team.KKR: 14,
+              Team.RR: 12}
 
-    nrr = {Team.CSK: 0.421,
-           Team.SRH: 0.448,
-           Team.RCB: -0.361,
-           Team.MI: 0.070,
-           Team.KXIP: 0.097,
-           Team.DD: -0.411,
-           Team.KKR: 0.145,
-           Team.RR: -0.552}
+    nrr = {Team.CSK: 0.383,
+           Team.SRH: 0.400,
+           Team.RCB: 0.218,
+           Team.MI: 0.405,
+           Team.KXIP: -0.518,
+           Team.DD: -0.478,
+           Team.KKR: -0.096,
+           Team.RR: -0.399}
 
     adjusted_points = {}
 
-    for team in points.keys():
-        adjusted_points[team] = points[team] + nrr[team] / 10
+    def __init__(self):
+        self.reset_adjusted_points()
+
+    def reset_adjusted_points(self):
+        for team in self.points.keys():
+            self.adjusted_points[team] = self.points[team] + self.nrr[team] / 100
 
     fixture = {35: (Team.CSK, Team.RCB),
                36: (Team.SRH, Team.DD),
@@ -74,9 +78,11 @@ class Simulator:
     qualify_counter = {}
     qualify_with_nrr_counter = {}
     qualify_with_current_nrr_counter = {}
+    qualify_top2_counter = {}
+    qualify_top2_with_nrr_counter = {}
+    log_all_combo = False
 
     max_log_non_qualifying = 10
-
 
     def __init__(self):
         self.reset()
@@ -93,6 +99,9 @@ class Simulator:
                                 Team.RR: 0}
         self.qualify_with_nrr_counter = copy.deepcopy(self.qualify_counter)
         self.qualify_with_current_nrr_counter = copy.deepcopy(self.qualify_counter)
+        self.qualify_with_nrr_counter = copy.deepcopy(self.qualify_counter)
+        self.qualify_top2_counter = copy.deepcopy(self.qualify_counter)
+        self.qualify_top2_with_nrr_counter = copy.deepcopy(self.qualify_counter)
 
     def play(self, chain, match_id, log_non_qualifying_team=None):
         if match_id > self.c.MAX_FIXTURE:
@@ -111,24 +120,24 @@ class Simulator:
         chain.pop()
 
     def play_team_domination(self, chain, match_id, team):
-            if match_id > self.c.MAX_FIXTURE:
-                self.evaluate_chain(chain)
-                return
+        if match_id > self.c.MAX_FIXTURE:
+            self.evaluate_chain(chain)
+            return
 
-            next_match_id = match_id + 1
-            fix = self.c.fixture[match_id]
+        next_match_id = match_id + 1
+        fix = self.c.fixture[match_id]
 
-            if fix[0] == team or fix[1] == team:
-                chain.append(team)
-                self.play(chain, next_match_id)
-            else:
-                chain.append(fix[0])
-                self.play(chain, next_match_id)
+        if fix[0] == team or fix[1] == team:
+            chain.append(team)
+            self.play(chain, next_match_id)
+        else:
+            chain.append(fix[0])
+            self.play(chain, next_match_id)
 
-                chain.pop()
-                chain.append(fix[1])
-                self.play(chain, next_match_id)
-                chain.pop()
+            chain.pop()
+            chain.append(fix[1])
+            self.play(chain, next_match_id)
+            chain.pop()
 
     def evaluate_chain(self, chain, log_non_qualifying_team=None):
         # global c
@@ -141,21 +150,32 @@ class Simulator:
             points_chain[winner] += 2
             adjusted_points_chain[winner] += 2
 
+        if self.log_all_combo:
+            self.log_scenario(chain, points_chain)
+
         # evaluate top 4
         sorted_points_chain = sorted(points_chain.items(), key=operator.itemgetter(1), reverse=True)
         # qualifying with NRR : keep adding to bucket top 4 items, then add any other matching #4's score
         # qualifying without NRR : add if points
 
         fourth_team_point = sorted_points_chain[3][1]
+        second_team_point = sorted_points_chain[1][1]
         qualifying_without_nrr = []
         qualifying_without_nrr_candidate = []
+        qualifying_top2_without_nrr = []
+        qualifying_top2_without_nrr_candidate = []
+        # not_qual_nrr = {}
+
         for i in range(0, len(sorted_points_chain)):
             team = sorted_points_chain[i][0]
             point = sorted_points_chain[i][1]
             # qualifying with NRR : teams 1-4, plus any team tieing with 4
             if i < 4 or point == fourth_team_point:
                 self.qualify_with_nrr_counter[team] += 1
-
+            if i < 2 or point == second_team_point:
+                self.qualify_top2_with_nrr_counter[team] += 1
+            # else:
+            #     not_qual_nrr[team] = 1
             # qualifying without NRR:
             # if point > 4th team point, then add to qualifying bucket
             # if point = 4th team point then add to candidate bucket
@@ -164,54 +184,94 @@ class Simulator:
             elif point == fourth_team_point:
                 qualifying_without_nrr_candidate.append(team)
 
+            if point > second_team_point:
+                qualifying_top2_without_nrr.append(team)
+            elif point == second_team_point:
+                qualifying_top2_without_nrr_candidate.append(team)
+
         # if qualifying + candidate = 4 then add, else discard candidate
         if len(qualifying_without_nrr) + len(qualifying_without_nrr_candidate) == 4:
             qualifying_without_nrr += qualifying_without_nrr_candidate
-
         for team in qualifying_without_nrr:
             self.qualify_counter[team] += 1
+
+        if len(qualifying_top2_without_nrr) + len(qualifying_top2_without_nrr_candidate) == 2:
+            qualifying_top2_without_nrr += qualifying_top2_without_nrr_candidate
+        for team in qualifying_top2_without_nrr:
+            self.qualify_top2_counter[team] += 1
 
         if log_non_qualifying_team is not None and log_non_qualifying_team not in qualifying_without_nrr:
             self.log_non_qualifying_scenario(chain, points_chain)
 
         # using current NRR as trend
-        for t in sorted(adjusted_points_chain.items(), key=operator.itemgetter(1), reverse=True)[:4]:
+        sorted_adjusted_points_chain = sorted(adjusted_points_chain.items(), key=operator.itemgetter(1), reverse=True)
+        for t in sorted_adjusted_points_chain[:4]:
             self.qualify_with_current_nrr_counter[t[0]] += 1
+            # if t[0] in not_qual_nrr:
+            #     self.log_scenario(chain, points_chain)
 
-    def print_log_non_qualifying_header(self):
+
+    @staticmethod
+    def get_header_dashes(txt):
+        return re.sub("[^|]", "-", txt)
+
+    def print_log_scenario_header(self):
         header = "|".join(self.c.points.keys())
         for match in range(CURRENT_MATCH, 57):
             header += "|" + str(match)
         print(header)
-        print(re.sub("[^|]","-",header))
-
+        print(self.get_header_dashes(header))
 
     def log_non_qualifying_scenario(self, chain, points_chain):
         self.max_log_non_qualifying -= 1
         if self.max_log_non_qualifying < 0:
             return
+        self.log_scenario(chain, points_chain)
+
+    def log_scenario(self, chain, points_chain):
         for team in self.c.points.keys():
             print(points_chain[team], end="|")
         print("|".join(chain))
-
 
     def print_chain(self, chain):
         for c in chain:
             print(c[0], end="  ")
 
-    def print_result(self, highlight_team=None):
-        print("\n\nTeam | Points | Qualifying | Qualifying Current NRR | Qualifying Higher NRR | Qualifying Scenarios | Current NRR | Higher NRR")
-        print("-----|--------|------------|-----------------------------|----------------------------|---------------|-------------|-----------")
-        for qt in sorted(self.qualify_counter.items(), key=operator.itemgetter(1), reverse=True):
+    def print_result(self, highlight_team=None, match_offset=CURRENT_MATCH):
+        header = "Team | Pt | ^(Matches Left) | ^(Qualifying) | ^(Qualifying Current NRR) | ^(Qualifying Higher NRR) | ^(Top 2) | ^(Qualifying Scenarios) | ^(Current NRR) | ^(Higher NRR) | ^(Top2 no tie) | ^(Top2 NRR)"
+        print("\n\n" + header)
+        print(self.get_header_dashes(header))
+        sorting_table = {}
+        for team in self.c.points.keys():
+            sorting_table.setdefault(team, (self.qualify_counter[team],
+                                            self.qualify_with_current_nrr_counter[team],
+                                            self.qualify_with_nrr_counter[team],
+                                            self.qualify_top2_counter[team],
+                                            self.qualify_top2_with_nrr_counter[team]))
+
+        sorted_table = sorted(sorting_table.items(), key=lambda x: (x[1][0], x[1][1], x[1][2], x[1][3], x[1][4]), reverse=True)
+        for qt in sorted_table:
             team = qt[0]
             team_txt = team if team != highlight_team else "**" + team + "**"
-            print(f"{team_txt} | {self.c.points[team]} |{self.get_percent(self.qualify_counter[team])}% | {self.get_percent(self.qualify_with_current_nrr_counter[team])}% | {self.get_percent(self.qualify_with_nrr_counter[team])}% | {self.qualify_counter[team]} | {self.qualify_with_current_nrr_counter[team]} | {self.qualify_with_nrr_counter[team]}")
+            top2 = 100 if self.get_percent(self.qualify_top2_counter[team]) == 100 else f"{self.get_percent(self.qualify_top2_counter[team])}-{self.get_percent(self.qualify_top2_with_nrr_counter[team])}"
+            print(f"{team_txt} | "
+                  f"{self.c.points[team]} | "
+                  f"{self.get_remaining_match_count(team, match_offset)} | "
+                  f"{self.get_percent(self.qualify_counter[team])}% | "
+                  f"{self.get_percent(self.qualify_with_current_nrr_counter[team])}% | "
+                  f"{self.get_percent(self.qualify_with_nrr_counter[team])}% | "
+                  f"{top2}% | "
+                  f"^{self.qualify_counter[team]} | "
+                  f"^{self.qualify_with_current_nrr_counter[team]} | "
+                  f"^{self.qualify_with_nrr_counter[team]} | "
+                  f"^{self.qualify_top2_counter[team]} | "
+                  f"^{self.qualify_top2_with_nrr_counter[team]}")
 
         print("\nTotal Simulations : ", self.total_counter, "\n")
-        
+
     def get_percent(self, x):
         percent = round(100 * x / self.total_counter)
-        if percent == 100 and x != self.total_counter:
+        if percent == 100 and x != self.total_counter or percent == 0 and x != 0:
             percent = round(100 * x / self.total_counter, 2)
         return percent
 
@@ -220,21 +280,26 @@ class Simulator:
         print(f"\n\n&nbsp;\n\nIf **{team1}** wins match {CURRENT_MATCH} {team1} vs {team2} : ")
         self.reset()
         self.c.points[team1] += 2
+        self.c.reset_adjusted_points()
         self.play([], CURRENT_MATCH + 1)
-        self.print_result(team1)
+        self.print_result(team1, CURRENT_MATCH + 1)
         self.c.points[team1] -= 2
 
         print(f"\n\n&nbsp;\n\nIf **{team2}** wins match {CURRENT_MATCH} {team1} vs {team2} : ")
         self.reset()
         self.c.points[team2] += 2
+        self.c.reset_adjusted_points()
         self.play([], CURRENT_MATCH + 1)
-        self.print_result(team2)
+        self.print_result(team2, CURRENT_MATCH + 1)
         self.c.points[team2] -= 2
+        self.c.reset_adjusted_points()
 
     def play_current(self, log_non_qualifying_team=None):
         if log_non_qualifying_team is not None:
             self.max_log_non_qualifying = 10
-            self.print_log_non_qualifying_header()
+            self.print_log_scenario_header()
+        if self.log_all_combo:
+            self.print_log_scenario_header()
         self.reset()
         self.play([], CURRENT_MATCH, log_non_qualifying_team)
         self.print_result()
@@ -244,21 +309,35 @@ class Simulator:
         self.play_team_domination([], CURRENT_MATCH, team)
         self.print_result()
 
+    def get_remaining_match_count(self, team, start_from=CURRENT_MATCH):
+        count = 0
+        for i in range(start_from, 57):
+            match = self.c.fixture[i]
+            if match[0] == team or match[1] == team:
+                count += 1
+        return count
+
 
 sim = Simulator()
+# sim.log_all_combo = True
 
-print(f"\n\n\nPlay off qualifying probability **after match {CURRENT_MATCH - 1} : {sim.c.fixture[CURRENT_MATCH-1][0]} vs {sim.c.fixture[CURRENT_MATCH-1][1]}**\n")
+print("\n\n\nx-post from [prediction](https://redd.it/8h36dv)\n")
+print(f"\nPlay off qualifying probability **after match {CURRENT_MATCH - 1} : {sim.c.fixture[CURRENT_MATCH-1][0]} vs {sim.c.fixture[CURRENT_MATCH-1][1]}**\n")
 # sim.play_current(log_non_qualifying_team=Team.SRH)
-
+#
 sim.play_current()
+
+sim.log_all_combo = False
 sim.play_whatif()
 print("\n\n&nbsp;\n\n^(**Legend:**)")
 print("\n\n^(1.**Qualifying** - team qualifies without any NRR tie breaker situation)")
-print("\n\n^(2.**Qualifying Current NRR** - team qualifies taking the current NRR as trend that is assumed to be the same after all the league matches)")
+print(
+    "\n\n^(2.**Qualifying Current NRR** - team qualifies taking the current NRR as trend that is assumed to be the same after all the league matches)")
 print("\n\n^(3.**Qualifying Higher NRR** - team qualifies owing to higher NRR)")
+print("\n\n^(4.**Top 2** - team finishes in top 2. The lower number represents no tie situation. The higher number represents higher NRR situation)")
 print("\n\n\n")
-#
-# sim.reset()
-# sim.play_team_winning_all_future(Team.RCB)
+
+sim.reset()
+# sim.play_team_winning_all_future(Team.RR)
 
 # sim.print_log_non_qualifying_header()
